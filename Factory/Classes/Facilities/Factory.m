c12d933327049795e0f831572c66c7e954e03bb2
erfilov <admin@iMac.local>
2016-02-21 01:24:28 +0600

    Removal Zombie-object

--- Factory/Classes/Facilities/Factory.m
+++ Factory/Classes/Facilities/Factory.m
@@ -245,7 +245,6 @@ static const NSUInteger DefaultLimitOnShipmentVolume = 5;
                     while (![rawMaterialStorage_ isFull]) {
                         [rawMaterialStorage_ putWare:[[[RawMaterial alloc] init] autorelease]];
                     }
-                    [error release];
                     error = nil;
                 }
             }
